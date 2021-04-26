e63d8bbfb2d9af687ac5fe145dbb967a91f8830f
ding0 <ding02211995@gmail.com>
2021-04-26 17:42:35 +0800

    add def turn_right & left

--- etch-a-sketch-start/main.py
+++ etch-a-sketch-start/main.py
@@ -9,7 +9,7 @@ def move_forwards():
 
 
 def back_forwards():
-    tim.forward(-10)
+    tim.backward(10)
 
 
 def counter_clockwise():
@@ -20,18 +20,29 @@ def clockwise():
     tim.circle(-60, 10)
 
 
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
 def clear_drawing():
     tim.clear()
     tim.penup()
-    tim.setpos(0, 0)
-    tim.setheading(0)
+    tim.home()
     tim.pendown()
 
 
 screen.listen()
-screen.onkey(key="W", fun=move_forwards)
-screen.onkey(key="S", fun=back_forwards)
-screen.onkey(key="A", fun=counter_clockwise)
-screen.onkey(key="D", fun=clockwise)
-screen.onkey(key="C", fun=clear_drawing)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=back_forwards)
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(key="q", fun=counter_clockwise)
+screen.onkey(key="e", fun=clockwise)
+screen.onkey(key="c", fun=clear_drawing)
 screen.exitonclick()
