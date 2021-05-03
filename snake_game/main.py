12a0fd50c168941fe84c88113fcc82e435b46680
ding0 <ding02211995@gmail.com>
2021-05-03 18:29:13 +0800

    Detect collision with your own tail.

--- snake_game/main.py
+++ snake_game/main.py
@@ -40,5 +40,13 @@ while game_is_on:
         game_is_on = False
         score_board.game_over()
 
+    # Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score_board.game_over()
+
 screen.exitonclick()
 
