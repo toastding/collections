6743fc9b0709b75cd106d6e35ad95e388b8ea727
ding0 <ding02211995@gmail.com>
2021-05-05 21:24:51 +0800

    Score keeping and changing the ball speed.

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(l_paddle.up, "w")
@@ -22,7 +24,7 @@ screen.onkey(r_paddle.down, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -37,10 +39,13 @@ while game_is_on:
     # Detect R_paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     # Detect L_paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
 
