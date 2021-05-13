8f8b06ee55dbbfea46a98233bca632d1ed6d9920
ding0 <ding02211995@gmail.com>
2021-05-13 21:05:06 +0800

    open, write, read file

---
+++ write_file/main.py
@@ -0,0 +1,9 @@
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+    file.close()
+
+
+with open("new_file.txt", mode="w") as file:
+    file.write("\nWhat's up!!")
+
