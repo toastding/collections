3aa8ba4068765445ce0f25f8fc4c7f48ffb97e59
ding0 <ding02211995@gmail.com>
2021-05-17 22:51:03 +0800

    Mail merge

--- write_file/main.py
+++ write_file/main.py
@@ -1,9 +1,10 @@
-with open("my_file.txt") as file:
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+#     file.close()
+#
+
+with open("../../../new_file.txt") as file:
     contents = file.read()
     print(contents)
-    file.close()
-
-
-with open("new_file.txt", mode="w") as file:
-    file.write("\nWhat's up!!")
 
