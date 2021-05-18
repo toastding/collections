f363e08dc06f75cf478bc4e9e9ff21e358ad2761
ding0 <ding02211995@gmail.com>
2021-05-18 22:54:16 +0800

    reading csv data using pandas

--- write_file/main.py
+++ write_file/main.py
@@ -1,8 +1,8 @@
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
-#     file.close()
-#
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+    file.close()
+
 
 with open("../../../new_file.txt") as file:
     contents = file.read()
