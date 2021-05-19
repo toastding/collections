b474c473ce93acef5dc2564a4d0eb69402e413d0
ding0 <ding02211995@gmail.com>
2021-05-19 19:38:20 +0800

    reading csv data using pandas

--- csv_practice/main.py
+++ csv_practice/main.py
@@ -15,4 +15,31 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data['temp'])
+# print(data['temp'])
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data['temp'].to_list()
+# print(sum(temp_list) / len(temp_list))
+# print(data['temp'].mean())
+# print(data['temp'].max())
+#
+# Get data in column
+# print(data['condition'])
+# print(data.condition)
+
+# Get data in row
+# print(data[data.day == "Tuesday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# print(int(monday.temp) * 1.8 + 32)
+
+# Create dataframe from scratch
+data_scratch = {
+    "students": ["Amy", "James", "Bob"],
+    "score": [94, 74, 89]
+}
+
+data = pandas.DataFrame(data_scratch)
+data.to_csv("new_data.csv")
