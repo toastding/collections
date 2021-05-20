7cbecd8d1e0132f4b2184ad359e0299c216fe471
ding0 <ding02211995@gmail.com>
2021-05-20 21:34:46 +0800

    saving data in .csv

--- us-states-game/us-states-game-start/main.py
+++ us-states-game/us-states-game-start/main.py
@@ -15,6 +15,14 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State",
                                     prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_state = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_state.append(state)
+        new_data = pandas.DataFrame(missing_state)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -24,8 +32,6 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(state_data.state.item())
 
-
-screen.exitonclick()
 
 
 # def get_mouse_click_coor(x, y):
