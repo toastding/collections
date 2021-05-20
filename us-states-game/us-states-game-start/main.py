c8afa986889fa0b2dd9b62dd8e0e1d8ef504ae8e
ding0 <ding02211995@gmail.com>
2021-05-20 21:21:18 +0800

    U.S. States Game with .csv

---
+++ us-states-game/us-states-game-start/main.py
@@ -0,0 +1,35 @@
+import turtle
+import pandas
+
+
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State",
+                                    prompt="What's another state's name?").title()
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
+
+screen.exitonclick()
+
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
