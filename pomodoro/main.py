b33390d22e678f7f13c8942cd69a82d02a08401b
ding0 <ding02211995@gmail.com>
2021-05-26 21:35:40 +0800

    Complete the application's user interface

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -16,14 +16,32 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+# window
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=30, pady=30, bg=YELLOW)
 
+# canvas
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(102, 134, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+# timer_label
+timer_label = Label(text="TIMER", font=(FONT_NAME, 50, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
+
+# check_label
+check_label = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_label.grid(column=1, row=3)
+
+# start_btn
+start_btn = Button(text="START", highlightthickness=0)
+start_btn.grid(column=0, row=2)
+
+# restart_btn
+reset_btn = Button(text="RESET", highlightthickness=0)
+reset_btn.grid(column=2, row=2)
 
 window.mainloop()
