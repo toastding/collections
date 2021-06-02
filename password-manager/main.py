52f2b7f5ddaf69c93029e418e37dd4b12127c475
ding0 <ding02211995@gmail.com>
2021-06-02 21:21:04 +0800

    searching for website & handling exceptions

--- password-manager/main.py
+++ password-manager/main.py
@@ -5,6 +5,22 @@ import pyperclip
 import json
 
 
+# ---------------------------- PASSWORD FIND ------------------------------- #
+def find_password():
+    web = web_entry.get()
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if web in data:
+            messagebox.showinfo(title="website",
+                                message=f"Email: {data[web]['email']}\nPassword: {data[web]['password']}")
+        else:
+            messagebox.showinfo(message="No details for the website exists.")
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -37,18 +53,22 @@ def save():
     if len(web) == 0 or len(password) == 0:
         messagebox.showinfo(message="Please don't leave any empty!")
     else:
-        with open("data.json", mode="r") as data_file:
-            # Reading old data
-            data = json.load(data_file)
-            # Updating old data with new data
-            data.update(new_data)
+        try:
+            with open("data.json", mode="r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+                # Updating old data with new data
+                data.update(new_data)
+            with open("data.json", mode="w") as data_file:
+                json.dump(data, data_file, indent=4)
 
-        with open("data.json", mode="w") as data_file:
-            # Saving updated data
-            json.dump(data, data_file, indent=4)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as data_file:
+                # Saving updated data
+                json.dump(new_data, data_file, indent=4)
 
-            web_entry.delete(0, END)
-            password_entry.delete(0, END)
+        web_entry.delete(0, END)
+        password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -74,8 +94,8 @@ email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
 
 # Entries
-web_entry = Entry(width=40)
-web_entry.grid(column=1, row=1, columnspan=2)
+web_entry = Entry(width=23)
+web_entry.grid(column=1, row=1)
 web_entry.focus()
 
 email_entry = Entry(width=40)
@@ -92,5 +112,8 @@ generate_btn.grid(column=2, row=3)
 
 add_btn = Button(text="Add", width=36, command=save)
 add_btn.grid(column=1, row=4, columnspan=2)
+
+search_btn = Button(text="Search", width=16, command=find_password)
+search_btn.grid(column=2, row=1)
 
 window.mainloop()
