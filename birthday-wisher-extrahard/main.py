0578b23358c15607c2e1cc48b49b21a19c47b17d
ding0 <ding02211995@gmail.com>
2021-06-09 21:51:35 +0800

    automated birthday wisher using dictionary comprehensive and tuple and data_itterrows

--- birthday-wisher-extrahard/main.py
+++ birthday-wisher-extrahard/main.py
@@ -1,38 +1,27 @@
 import random
 import smtplib
-
 import pandas
 import datetime
 
 MY_EMAIL = "shumingting0@gmail.com"
 MY_PASSWORD = "Star0258"
-PLACEHOLDER = "[NAME]"
-# random letter
-letter = random.randint(1, 4)
 
-# whose birthday
 data = pandas.read_csv("birthdays.csv")
-birth = data.to_dict(orient='records')
+birthday_dict = {(data_row['month'], data_row['day']): data_row for (index, data_row) in data.iterrows()}
 
-# today
-now = datetime.datetime.now()
-month = now.month
-day = now.day
+today = datetime.datetime.now()
+today_tuple = (today.month, today.day)
 
-# Check if today matches a birthday in the birthdays.csv
-for i in range(len(birth)):
-    if birth[i]['month'] == month and birth[i]['day'] == day:
-        print(birth[i])
-        # pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-        with open(f"letter_templates/letter_{letter}.txt") as file:
-            content = file.read()
-            new_letter = content.replace(PLACEHOLDER, birth[i]['name'])
-            # Send the letter generated in step 3 to that person's email address.
-            with smtplib.SMTP("smtp.gmail.com") as connection:
-                connection.starttls()
-                connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-                connection.sendmail(
-                    from_addr=MY_EMAIL,
-                    to_addrs=birth[i]["email"],
-                    msg=f"Subject:Happy Birthday!!!\n\n{new_letter}"
-                )
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    with open(f"letter_templates/letter_{random.randint(1, 3)}.txt") as file:
+        content = file.read()
+        new_letter = content.replace("[NAME]", birthday_person['name'])
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=birthday_person["email"],
+                msg=f"Subject:Happy Birthday!!!\n\n{new_letter}"
+            )
