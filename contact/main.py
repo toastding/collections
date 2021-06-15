0bf402ff6580ca6a739f7f039d86ce119a5defe7
ding0 <ding02211995@gmail.com>
2021-06-15 20:43:03 +0800

    send email to multiple address

--- contact/main.py
+++ contact/main.py
@@ -1,5 +1,4 @@
 import smtplib
-import random
 
 MY_EMAIL = "shumingting0@gmail.com"
 MY_PASSWORD = "Star0258"
@@ -8,6 +7,7 @@ final_contact = []
 result = []
 final_result = []
 SEND_EMAIL = ''
+email_list = []
 
 
 # def merge_dict(dict1, dict2):
@@ -24,12 +24,12 @@ def merge_list(list1, list2):
     if list1[0] == list2[0]:
         if list2[2] == list1[2]:
             list2[2] = list1[2]
-            if list2[3] == list1[3]:
-                list2[3] = list1[3]
-            else:
-                list2[3] = list1[3] + ',' + list2[3]
         else:
             list2[2] = list1[2] + ',' + list2[2]
+        if list2[3] == list1[3]:
+            list2[3] = list1[3]
+        else:
+            list2[3] = list1[3] + ',' + list2[3]
     return list2
 
 
@@ -38,55 +38,61 @@ def check_same(list1, list2):
         return True
 
 
+# open txt & apply to list
 with open("通訊錄考題1.txt", mode="r", encoding="utf-8") as file:
     data = file.readlines()
     for i in data:
         list_split = i.split()
         contact.append(list_split)
     new_contact = sorted(contact, key=lambda x: (x[0], x[2]))
-    # print(new_contact)
 
+
+# merge different information
     for i in range(len(new_contact)-1):
         final = merge_list(new_contact[i], new_contact[i+1])
         # print(final)
         final_contact.append(final)
 
-    # print(len(final_contact))
 
+# remove duplicated information
     for i in range(len(final_contact)-1):
         if check_same(final_contact[i], final_contact[i+1]):
             final_contact[i].clear()
 
+    list3 = [x for x in final_contact if x != []]
 
-    # print(final_contact)
+# apply to dic
 
-    list2 = [x for x in final_contact if x != []]
-    # print(list2)
-
-    for i in list2:
+with open('contact.txt', mode='w') as file:
+    for i in list3:
         contact_dic = {
             "name": i[0],
             "gender": i[1],
             "phone": i[2],
             "email": i[3]
         }
+        file.write(str(contact_dic) + '\n')
+        # print(contact_dic)
+
         result.append(contact_dic)
-    print(result)
+    # print(result)
 
 # -----------------------------------SMTP------------------------------------ #
-    name = input("Enter the name: ")
+    name = input("Enter the name: ").capitalize()
     for i in range(len(result)):
         if result[i]['name'] == name:
             SEND_EMAIL = result[i]['email']
-
-            with smtplib.SMTP("smtp.gmail.com") as connection:
-                connection.starttls()
-                connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-                connection.sendmail(
-                    from_addr=MY_EMAIL,
-                    to_addrs=SEND_EMAIL,
-                    msg=f"Subject:Information\n\n{result[i]['name'], result[i]['gender'], result[i]['phone'], result[i]['email']}"
-                )
+            email_list = SEND_EMAIL.split(',')
+            for item in email_list:
+                SEND_EMAIL = item
+                with smtplib.SMTP("smtp.gmail.com") as connection:
+                    connection.starttls()
+                    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+                    connection.sendmail(
+                        from_addr=MY_EMAIL,
+                        to_addrs=SEND_EMAIL,
+                        msg=f"Subject:Information\n\n{result[i]['name'], result[i]['gender'], result[i]['phone'], result[i]['email']}"
+                    )
 # -----------------------------------SMTP------------------------------------ #
 
 
