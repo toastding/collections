208f3f2100c2493892c2ab15ee0f939c05eaade1
ding0 <ding02211995@gmail.com>
2021-06-24 20:44:55 +0800

    Give Feedback to the Player, Keep Score and Fix the Bugs

--- quizzler-app/data.py
+++ quizzler-app/data.py
@@ -9,16 +9,5 @@ response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 
-question_data = [
-    {
-        "category": data['results'][0].get('category'),
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": data['results'][0].get('question'),
-        "correct_answer": data['results'][0].get('correct_answer'),
-        "incorrect_answers": [
-            data['results'][0].get('incorrect_answer')
-        ]
-    }
-]
+question_data = data["results"]
 
