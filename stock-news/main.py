682d6deef474327bda1c726f7a94d7566574bf2e
ding0 <ding02211995@gmail.com>
2021-07-19 15:12:13 +0800

    get the news articles

--- stock-news/main.py
+++ stock-news/main.py
@@ -14,8 +14,7 @@ NEWS_API = "f9ac8e903cd542bbbd0d61f45a94f09f"
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-# TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g.
-#  [new_value for (key, value) in dictionary.items()]
+# Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
 stock_params = {
     'function': STOCK_FUNCTION,
     'symbol': STOCK_NAME,
@@ -29,12 +28,11 @@ data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
 
-# TODO 2. - Get the day before yesterday's closing stock price
+# Get the day before yesterday's closing stock price
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data['4. close']
 
-# TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint:
-#  https://www.w3schools.com/python/ref_func_abs.asp
+# Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
 
 # if difference > 0:
@@ -43,40 +41,29 @@ difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_clo
 #     DIRECTION = "🔻"
 # positive = abs(difference)
 
-# TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price
-#  the day before yesterday.
+# Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
 diff_percent = (difference / float(day_before_yesterday_closing_price)) * 100
-
-# TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_percent > 5:
-    print('Get News')
 
 # STEP 2: https://newsapi.org/
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
-# TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-# https://newsapi.org/v2/everything?q=Apple&from=2021-07-19&sortBy=popularity&apiKey=API_KEY
-#
-#
-# news_params = {
-#     'q': COMPANY_NAME,
-#     'from': date[0],
-#     'sortBy': 'popularity',
-#     'apiKey': NEWS_API
-# }
-#
-# response = requests.get(NEWS_ENDPOINT, params=news_params)
-# response.raise_for_status()
-# news_data = response.json()
-#
-# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow
-#  .com/questions/509211/understanding-slice-notation
+# Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+if diff_percent > 0:
+    news_params = {
+        'q': COMPANY_NAME,
+        'from': yesterday_data,
+        'sortBy': 'popularity',
+        'apiKey': NEWS_API
+    }
 
-# stock_news = [news_data["articles"][:3]]
-#
-#
-# # STEP 3: Use twilio.com/docs/sms/quickstart/python
-# # to send a separate message with each article's title and description to your phone number.
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()['articles']
+
+# Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+    three_articles = articles[:3]
+
+# STEP 3: Use twilio.com/docs/sms/quickstart/python
+# to send a separate message with each article's title and description to your phone number.
 #
 # TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 # headline = [article['title'] for article in stock_news[0]]
@@ -86,7 +73,7 @@ if diff_percent > 5:
 # print(f'{STOCK_NAME}: {DIRECTION}{percentage}%\nHeadline: {headline[0]}\nBrief: {description[0]}')
 #
 #
-#Optional TODO: Format the message like this:
+# Optional TODO: Format the message like this:
 
 """
 TSLA: 🔺2%
