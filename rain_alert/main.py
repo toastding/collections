c5dd67020750abf64bba72142d739b522fd16b8c
ding0 <ding02211995@gmail.com>
2021-07-19 15:36:39 +0800

    send sms message

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -4,7 +4,7 @@ from twilio.rest import Client
 OWH_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "678a48e8761ea1e358eed541ecc2e716"
 account_sid = "ACe1ae4e388a1a7390b292e744d125d13f"
-auth_token = "cc19cc9ad842e61bcd100b5429efa52f"
+auth_token = "36149300860710b127c0a038229f2eeb"
 
 
 weather_params = {
--- stock-news/main.py
+++ stock-news/main.py
@@ -9,7 +9,8 @@ STOCK_API_KEY = "3DTVXRGP7951VIZT"
 STOCK_FUNCTION = "TIME_SERIES_DAILY"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 NEWS_API = "f9ac8e903cd542bbbd0d61f45a94f09f"
-
+account_sid = "ACe1ae4e388a1a7390b292e744d125d13f"
+auth_token = "36149300860710b127c0a038229f2eeb"
 
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -33,22 +34,23 @@ day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data['4. close']
 
 # Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
 
-# if difference > 0:
-#     DIRECTION = "🔺"
-# else:
-#     DIRECTION = "🔻"
-# positive = abs(difference)
+if difference > 0:
+    DIRECTION = "🔺"
+else:
+    DIRECTION = "🔻"
+
+positive = abs(difference)
 
 # Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference / float(day_before_yesterday_closing_price)) * 100
+diff_percent = round((positive / float(day_before_yesterday_closing_price)) * 100)
 
 # STEP 2: https://newsapi.org/
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 # Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-if diff_percent > 0:
+if diff_percent > 1:
     news_params = {
         'q': COMPANY_NAME,
         'from': yesterday_data,
@@ -62,26 +64,17 @@ if diff_percent > 0:
 # Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
     three_articles = articles[:3]
 
-# STEP 3: Use twilio.com/docs/sms/quickstart/python
-# to send a separate message with each article's title and description to your phone number.
-#
-# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-# headline = [article['title'] for article in stock_news[0]]
-# description = [article['description'] for article in stock_news[0]]
-#
-# TODO 9. - Send each article as a separate message via Twilio.
-# print(f'{STOCK_NAME}: {DIRECTION}{percentage}%\nHeadline: {headline[0]}\nBrief: {description[0]}')
-#
-#
-# Optional TODO: Format the message like this:
+# STEP 3: Use twilio.com/docs/sms/quickstart/python to send a separate message with each article's title and description to your phone number.
 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+# Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [f"{STOCK_NAME}: {DIRECTION}{diff_percent}% \nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
 
+# Send each article as a separate message via Twilio.
+
+    client = Client(account_sid, auth_token)
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_='+17742284856',
+            to='+886978199305'
+        )
