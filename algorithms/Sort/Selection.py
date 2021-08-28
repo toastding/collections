995463c67753544c8170e533e0d98cb23d426040
ding0 <ding02211995@gmail.com>
2021-08-28 22:06:12 +0800

    add Selection Sort

---
+++ algorithms/Sort/Selection.py
@@ -0,0 +1,26 @@
+def sel_sort(self, n):
+    for i in range(n):
+        np = i
+        for j in range(i + 1, n):
+            if self[j] > self[np]:
+                np = j
+        temp = self[i]
+        self[i] = self[np]
+        self[np] = temp
+    return self
+
+
+b = [22, 49, 2, 10, 6]
+n = len(b)
+
+print(sel_sort(b, n))
+
+'''
+[49, 22, 2, 10, 6] i = 0
+
+[49, 22, 2, 10, 6] i = 1
+
+[49, 22, 10, 2, 6] i = 2
+
+[49, 22, 10, 6, 2] i = 3
+'''
